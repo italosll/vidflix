39d3e0b380b7652130f114842d164c31db0eef6d
Italo Silva <italomsilva.if@gmail.com>
2020-07-28 15:07:12 -0300

    Carrosel  Implementado

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home/index.js';
+import CadastroVideo from './pages/cadastro/video/index.js';
+
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroCategoria from './pages/cadastro/categoria';
+
+
+
+
+const pagina404 = (<div>Erro 404</div>);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact/>
+      <Route path="/cadastro/video" component={CadastroVideo}/>
+      <Route path="/cadastro/categoria" component={CadastroCategoria}/>
+
+      <Route component={() => pagina404}/>
+
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
